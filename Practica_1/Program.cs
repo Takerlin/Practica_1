d71ec046b284ec83548f4c8a33ea227444d3b89d
Takerlin Gereidi <Takerlin Gereidi@DESKTOP-15BE5S5>
2022-09-01 19:17:59 -0700

    Agregar archivos de proyecto.

---
+++ Practica_1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Takerlin Gereidi Calderon Corvera");
+            Console.ReadKey();
+            Console.WriteLine("Mexicali, Baja California");
+            Console.ReadKey();
+            Console.WriteLine("20");
+            Console.ReadKey();
+        }
+    }
+}
